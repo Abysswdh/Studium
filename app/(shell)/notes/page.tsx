0b30ff6a7a2a5fae0dea8508bb815ce072a34f4a
Abysswdh <putraabyasawedha@gmail.com>
2026-03-18 00:41:45 +0800

    Wrap notes hub page in Suspense

--- app/(shell)/notes/page.tsx
+++ app/(shell)/notes/page.tsx
@@ -1,11 +1,14 @@
 import NotesHub from "../../../components/notes/notes-hub";
 import ViewMarker from "../../../components/view-marker";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <>
       <ViewMarker view="notes" />
-      <NotesHub />
+      <Suspense fallback={null}>
+        <NotesHub />
+      </Suspense>
     </>
   );
 }
